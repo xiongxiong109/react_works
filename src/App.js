32d4867c4f353e63323a733ce7f05a89b6924bdf
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-06-01 20:29:27 +0800

    update app

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import React, { Component } from 'react';
 import RandomList from './components/RandomList'
 
 import 'thanos/dist/main.css'
-// import { TButton, TInput } from 'thanos'
+import { TButton, TInput } from 'thanos'
 
 import 'thanos/dist/SearchInput/SearchInput.css'
+import 'thanos/dist/SearchCity/SearchCity.css'
 import SearchInput from 'thanos/dist/SearchInput/SearchInput'
+import SearchCity from 'thanos/dist/SearchCity/SearchCity'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <SearchInput />
+        <SearchCity />
         <RandomList />
         <p>{'bottom'}</p>
       </div>
