b874538192b013303c9a0a368bc73ce6c73caf8f
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-06-28 11:20:17 +0800

    add modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Modal from './components.base/Modal/modal'
 import RandomList from './components/RandomList'
 
 import 'thanos/dist/main.css'
@@ -11,18 +11,49 @@ import SearchInput from 'thanos/dist/SearchInput/SearchInput'
 import SearchCity from 'thanos/dist/SearchCity/SearchCity'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: {
+        isShow: false,
+        title: '',
+        content: ''
+      }
+    }
+  }
   render() {
+    const { modal } = this.state;
     return (
       <div className="App">
         <SearchInput />
         <SearchCity />
         <RandomList />
-        <p>{'bottom'}</p>
+        <button onClick={() => this.showSth()}>showModal</button>
+        <Modal {...modal} />
       </div>
     );
   }
-  componentDidMount() {
-    // console.log(this.refs.counter.state)
+  showSth() {
+    const _view = this;
+    this.showModal('hello world', 'bear xiong', {
+      onClickOutside(ev) {
+        _view.setState({
+          modal: Object.assign({}, _view.state.modal, {
+            isShow: false
+          })
+        })
+      }
+    })
+  }
+  showModal(content, title, opts = {}) {
+    this.setState({
+      modal: Object.assign({}, this.state.modal, {
+        isShow: true,
+        title,
+        content,
+        ...opts
+      })
+    })
   }
 }
 
---
+++ src/components.base/Modal/modal.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { createPortal } from 'react-dom'
+import './modal.css'
+
+export default class Modal extends Component {
+    static propTypes = {
+        isShow: PropTypes.bool,
+        title: PropTypes.string,
+        content: PropTypes.string,
+        onClickOutside: PropTypes.func
+    }
+    static defaultProps = {
+        isShow: false,
+        title: '我是标题',
+        content: '',
+        onClickOutside: null
+    }
+    render() {
+        
+        const {
+            isShow,
+            title,
+            content,
+            onClickOutside
+        } = this.props;
+
+        return createPortal(
+            <div
+                className="app__modal--wrapper"
+                style={
+                    {
+                        display: `${isShow ? 'block': 'none'}`
+                    }
+                }
+            >
+                <div className="app__modal--layer" onClick={onClickOutside}></div>
+                <div className="app__modal--container">
+                    <div className="app__modal--title">{ title }</div>
+                    <div className="app__modal--content">{ content }</div>
+                </div>
+            </div>,
+        document.body)
+    }
+}
